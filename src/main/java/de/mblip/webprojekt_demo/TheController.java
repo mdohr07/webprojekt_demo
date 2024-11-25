b3f7caa69391d1af2fb5fe94b0f3902b4cda44fe
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-11-25 16:22:10 +0100

    added some sample content

--- src/main/java/de/mblip/webprojekt_demo/TheController.java
+++ src/main/java/de/mblip/webprojekt_demo/TheController.java
@@ -1,18 +1,27 @@
 package de.mblip.webprojekt_demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TheController {
 
-@GetMapping("/greeting")
+    @GetMapping("/greeting")
     public String goodMorning() {
         return "hello_world"; // Entspricht hello_world.html
     }
 
     @GetMapping("/helloThymeleaf")
     public String helloThymeleaf() {
-        return "hello_world_thymeleaf";
+        return "hello_thymeleaf";
     }
+
+    @GetMapping("/strings")
+    public String strings(Model model) {
+        String txt = "Some text"; // <p th:text="${text}" /> zeigt jetzt "Some text"
+        model.addAttribute("text", txt); //Der String ist ein Key für eine Variable
+        return "strings";
+    }
+
 }

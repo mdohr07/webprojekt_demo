e854d5f86125a0f104a28ca4b9811934262137f7
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-11-25 11:27:06 +0100

    project files added

---
+++ src/main/java/de/mblip/webprojekt_demo/WebprojektDemoApplication.java
@@ -0,0 +1,13 @@
+package de.mblip.webprojekt_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojektDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebprojektDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/mblip/webprojekt_demo/WebprojektDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mblip.webprojekt_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojektDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
